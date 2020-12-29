d936df4668ec1ef647a8ed6ad686414dd326e16a
Allef Douglas <allef.douglas@joyjet.com>
2020-12-29 14:28:21 -0300

    Adding dialectModuto to database config

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize'
+import mysql2 from 'mysql2'
 
 export const database = new Sequelize('plustech', 'root', 'password', {
   host: 'localhost',
   dialect: 'mysql',
+  dialectModule: mysql2,
   define: {
     timestamps: true,
     underscored: true,
